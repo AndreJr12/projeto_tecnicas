7a031197f741d5eb48d9a3514a78a36aa1462308
Auto Logon <autologon@CSED.LAB>
2023-04-04 21:34:45 -0300

    'Projeto'

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+const notebooks = [
+
+    {"id":1, "marca":"Asus"},
+    {"id":2, "marca":"HP" }
+
+]
+
+
+app.get('/' , (req, res) =>{
+    res.status(200).send ('API Loja');
+} )
+
+app.get('/note', (res,  res) => {
+    res.status(200).json(notebooks)
+})
+
+export default app;
+ 
